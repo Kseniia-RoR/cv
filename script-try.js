afa8d595caa9b4cfc0046c6dc704177496eeaa87
Kseniia_Markiv <ksenya.markiv@gmail.com>
2022-12-27 02:28:37 +0200

    remove couple files

--- script-try.js
+++
@@ -1,7 +0,0 @@
-let config = {
-    'strokeWidth': 2, // Adjust width of stroke
-    'strokeColor': '#F7C429'
-}
-let svg = document.querySelector('#my-svg')
-let myAnimation = new LazyLinePainter(svg, config)
-myAnimation.paint()
